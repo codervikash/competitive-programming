eba81fc1766bc95f977a4f9bd231f5218ba07376
Vikash Kumar <mailkumarvikash@gmail.com>
2017-09-18 22:02:02 +0530

    reverse string

---
+++ InterviewBit/Strings/reverse_the_string.py
@@ -0,0 +1,35 @@
+"""
+Given an input string, reverse the string word by word.
+
+Example:
+
+Given s = "the sky is blue",
+
+return "blue is sky the".
+
+A sequence of non-space characters constitutes a word.
+Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+If there are multiple spaces between words, reduce them to a single space in the reversed string.
+"""
+
+from collections import deque
+
+class Solution:
+    # @param A : string
+    # @return string
+    def reverseWords(self, A):
+        l = len(A)
+        stack = deque()
+        result = ''
+        for i in xrange(l - 1, -1, -1):
+            if A[i] != ' ':
+                stack .appendleft(A[i])
+            else:
+                while stack:
+                    result += stack.popleft()
+                result += ' '
+
+        while stack:
+            result += stack.popleft()
+
+        return result.strip()
