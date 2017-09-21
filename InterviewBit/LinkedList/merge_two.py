c1c19ea9a37777f3d6e2dbeab59b649d2280ba2b
Vikash Kumar <mailkumarvikash@gmail.com>
2017-09-21 20:32:02 +0530

    merge two

---
+++ InterviewBit/LinkedList/merge_two.py
@@ -0,0 +1,43 @@
+"""
+Merge two sorted linked lists and return it as a new list.
+The new list should be made by splicing together the nodes of the first two lists, and should also be sorted.
+
+For example, given following linked lists :
+
+  5 -> 8 -> 20
+  4 -> 11 -> 15
+The merged list should be :
+
+4 -> 5 -> 8 -> 11 -> 15 -> 20
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @param B : head node of linked list
+    # @return the head node in the linked list
+    def mergeTwoLists(self, A, B):
+        merge_node = ListNode(0)
+        merge_list = merge_node
+
+        while A is not None and B is not None:
+            if A.val < B.val:
+                merge_list.next = A
+                A = A.next
+            else:
+                merge_list.next = B
+                B = B.next
+
+            merge_list = merge_list.next
+
+        if A is not None:
+            merge_list.next = A
+        elif B is not None:
+            merge_list.next = B
+
+        return merge_node.next
